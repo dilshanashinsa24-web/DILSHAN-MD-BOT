7e2305e1fe342a9d7a95af4cde7500ab9bf0c448
dilshanashinsa24-web <dilshanashinsa24@gmail.com>
2026-06-23 20:29:48 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    // Bot Settings
+    OWNER_NUMBER: "94772194789", // උඹේ number එක +94 නැතුව. Ex: 94771234567
+    BOT_NAME: "Dilshan MD Bot",
+    PREFIX: ".",
+    
+    // Message Settings  
+    WELCOME_MSG: "🎵 Songs Bot Online! .help ගහපන්",
+    THUMBNAIL_URL: "https://i.imgur.com/2nCt3Sbl.jpg",
+    
+    // Auto Features
+    AUTO_READ_STATUS: false,
+    AUTO_READ_MSG: false
+}
